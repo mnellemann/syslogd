5104bd07506d08baf62d6ea48d5580fc9963499f
Mark Nellemann <mark.nellemann@gmail.com>
2022-12-13 16:59:28 +0100

    Work on chunked GELF messages.

--- src/main/java/biz/nellemann/syslogd/parser/GelfParser.java
+++ src/main/java/biz/nellemann/syslogd/parser/GelfParser.java
@@ -4,13 +4,12 @@ import biz.nellemann.syslogd.msg.SyslogMessage;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.collections4.map.PassiveExpiringMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.UnsupportedEncodingException;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.zip.DataFormatException;
+import java.util.*;
 
 public class GelfParser extends SyslogParser {
 
@@ -18,9 +17,60 @@ public class GelfParser extends SyslogParser {
 
     private final ObjectMapper objectMapper;
 
+    private final int expiryInMills = 10_000;
+    private final PassiveExpiringMap<Integer, TreeMap<Integer, byte[]>> expiringMap = new PassiveExpiringMap<>(expiryInMills);
+
+
     public GelfParser() {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+    }
+
+
+    /*
+        Magic Bytes - 2 bytes: 0x1e 0x0f
+        Message ID - 8 bytes: Must be the same for every chunk of this message.
+            Identifies the whole message and is used to reassemble the chunks later.
+            Generate from millisecond timestamp + hostname, for example.
+        Sequence number - 1 byte: The sequence number of this chunk starts at 0 and is always less than the sequence count.
+        Sequence count - 1 byte: Total number of chunks this message has.
+
+        All chunks MUST arrive within 5 seconds or the server will discard all chunks that have arrived or are in the process of arriving. A message MUST NOT consist of more than 128 chunks.
+    */
+    private SyslogMessage parseChunked(byte[] input) {
+
+        if(input.length < 12) return null;
+
+        byte[] messageId = { input[2], input[3], input[4], input[5], input[6], input[7], input[8], input[9] };
+        byte seqNumber = input[10];
+        byte seqTotal = input[11];
+        byte[] payload = Arrays.copyOfRange(input, 12, input.length);
+        log.debug("parseChunked() - msgId: {}, seqNo: {}, seqTot: {}, payload: {}", messageId, seqNumber, seqTotal, byteArrayToString(payload));
+
+        // messageId byte[] to int
+        int id = 0;
+        for (byte b : messageId) {
+            id = (id << 8) + (b & 0xFF);
+        }
+
+        TreeMap<Integer, byte[]> integerTreeMap;
+        if(expiringMap.containsKey(id)) {
+            integerTreeMap = expiringMap.get(id);
+        } else {
+            integerTreeMap = new TreeMap<>();
+        }
+        integerTreeMap.put((int)seqNumber, payload);
+        expiringMap.put(id, integerTreeMap);
+
+        if(seqNumber+1 >= seqTotal) {
+            StringBuilder sb = new StringBuilder();
+            integerTreeMap.forEach( (i, p) -> {
+                sb.append(byteArrayToString(p));
+            });
+            return parse(sb.toString());
+        }
+
+        return null;
     }
 
 
@@ -56,34 +106,25 @@ public class GelfParser extends SyslogParser {
     @Override
     public SyslogMessage parse(byte[] input) {
 
-        if(input.length < 8) return null;   // TODO: Find proper minimum input length ?
-        String text;
 
-        // GELF Magic Bytes: 0x1e 0x0f
-        if(input[0] == (byte)0x1e && input[1] == (byte)0x0f) {
-            /*
-                Message ID - 8 bytes: Must be the same for every chunk of this message.
-                    Identifies the whole message and is used to reassemble the chunks later.
-                    Generate from millisecond timestamp + hostname, for example.
-                Sequence number - 1 byte: The sequence number of this chunk starts at 0 and is always less than the sequence count.
-                Sequence count - 1 byte: Total number of chunks this message has.
-
-                All chunks MUST arrive within 5 seconds or the server will discard all chunks that have arrived or are in the process of arriving. A message MUST NOT consist of more than 128 chunks.
-             */
-            log.warn("parse() - Found Magic Bytes, can't parse yet.");
-            byte[] newInput = Arrays.copyOfRange(input, 12, input.length);
-            //return parse(byteArrayToString(newInput));
-            return null;
+        for(byte b : input) {
+            if(b > 0x0) {
+                System.out.printf("%d, ", (b & 0xff));
+            }
         }
+        System.out.println();
+
+
+        if(input.length < 8) return null;   // TODO: Find proper minimum input length ?
 
         // Compressed data: 0x78 0x9c
-        if(input[0] == (byte)0x78 && input[1] == (byte)0x9c) {  // TODO: better detection ?
-            try {
-                return parse(decompress(input));
-            } catch (DataFormatException | UnsupportedEncodingException e) {
-                log.error("parse() - error: {}", e.getMessage());
-                return null;
-            }
+        if(input[0] == (byte)0x78 && input[1] == (byte)0x9c) {
+            input = decompress(input);
+        }
+
+        // Magic Bytes: 0x1e 0x0f
+        if(input[0] == (byte)0x1e && input[1] == (byte)0x0f) {
+            return parseChunked(input);
         }
 
         return parse(byteArrayToString(input));
